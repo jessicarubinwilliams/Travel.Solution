1810048f475ef7064278fbba167d7384edaa2563
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-11-16 19:35:35 -0800

    Add migration files
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>

---
+++ Travel/Migrations/20211117030133_AddJoinEntity.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations
+{
+    public partial class AddJoinEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Review",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "VisitDate",
+                table: "Destinations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Destinations",
+                type: "varchar(50) CHARACTER SET utf8mb4",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Destinations",
+                type: "varchar(56) CHARACTER SET utf8mb4",
+                maxLength: 56,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Destinations",
+                type: "varchar(85) CHARACTER SET utf8mb4",
+                maxLength: 85,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    VisitDate = table.Column<string>(type: "varchar(85) CHARACTER SET utf8mb4", maxLength: 85, nullable: false),
+                    Rating = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Comments = table.Column<string>(type: "varchar(200) CHARACTER SET utf8mb4", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DestinationReview",
+                columns: table => new
+                {
+                    DestinationReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DestinationId = table.Column<int>(type: "int", nullable: false),
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DestinationReview", x => x.DestinationReviewId);
+                    table.ForeignKey(
+                        name: "FK_DestinationReview_Destinations_DestinationId",
+                        column: x => x.DestinationId,
+                        principalTable: "Destinations",
+                        principalColumn: "DestinationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DestinationReview_Reviews_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "Reviews",
+                        principalColumn: "ReviewId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationReview_DestinationId",
+                table: "DestinationReview",
+                column: "DestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationReview_ReviewId",
+                table: "DestinationReview",
+                column: "ReviewId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DestinationReview");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Destinations",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50) CHARACTER SET utf8mb4",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Destinations",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(56) CHARACTER SET utf8mb4",
+                oldMaxLength: 56);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Destinations",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(85) CHARACTER SET utf8mb4",
+                oldMaxLength: 85);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Destinations",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Review",
+                table: "Destinations",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VisitDate",
+                table: "Destinations",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
